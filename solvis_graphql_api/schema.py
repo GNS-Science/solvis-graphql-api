0ea9dd262b7bb2e56b4ba4dce9d145a5c606390b
Chris Chamberlain <chrisbc@artisan.co.nz>
2023-01-19 12:28:10 +1300

    initial geojson resolver working

--- solvis_graphql_api/schema.py
+++ solvis_graphql_api/schema.py
@@ -2,20 +2,24 @@
 
 import logging
 
+import geopandas as gpd
 import graphene
 from graphene import relay
 
-from nzshm_common.location import CodedLocation
+# from nzshm_common.location import CodedLocation
+from solvis_store.solvis_db_query import matched_rupture_sections_gdf
 
 log = logging.getLogger(__name__)
 
 
 class InversionSolutionRupture(graphene.ObjectType):
     fault_id = graphene.Int(description="Unique ID of the rupture within this solution")
-    magnitude = graphene.Float(description='rupture magnitude' )
+    magnitude = graphene.Float(description='rupture magnitude')
+
 
 class InversionSolutionFaultSection(graphene.ObjectType):
     fault_id = graphene.String(description="Unique ID of the fault section eg WHV1")
+
 
 class InversionSolutionAnalysis(graphene.ObjectType):
     """Represents the internal details of a given solution or filtered solution"""
@@ -25,29 +29,33 @@ class InversionSolutionAnalysis(graphene.ObjectType):
     ruptures = graphene.List(InversionSolutionRupture)
     geojson = graphene.JSONString()
 
+
 class InversionSolutionAnalysisArguments(graphene.InputObjectType):
     """Defines filter arguments for Inversions analysis"""
 
     solution_id = graphene.ID(required=True, description="The ID of the InversionSolution")
-    location_codes = graphene.List(graphene.String, required=False, description="Optional list of locations codes for proximity filtering e.g. `WLG,PMR,ZQN`")
-    proximity_km = graphene.Int( required=False, description = 'The rupture/location intersection radius in km')
-    min_rate = graphene.Float(required=False, description="Constrain to ruptures having a annual rate above the value supplied.")
-    max_rate = graphene.Float(required=False, description="Constrain to ruptures having a annual rate below the value supplied.")
-    min_mag = graphene.Float(required=False, description="Constrain to ruptures having a magnitude above the value supplied.")
-    max_mag = graphene.Float(required=False, description="Constrain to ruptures having a magnitude below the value supplied.")
+    location_codes = graphene.List(
+        graphene.String,
+        required=False,
+        description="Optional list of locations codes for proximity filtering e.g. `WLG,PMR,ZQN`",
+    )
+    radius_km = graphene.Int(required=False, description='The rupture/location intersection radius in km')
+    minimum_rate = graphene.Float(
+        required=False, description="Constrain to ruptures having a annual rate above the value supplied."
+    )
+    maximum_rate = graphene.Float(
+        required=False, description="Constrain to ruptures having a annual rate below the value supplied."
+    )
+    minimum_mag = graphene.Float(
+        required=False, description="Constrain to ruptures having a magnitude above the value supplied."
+    )
+    maximum_mag = graphene.Float(
+        required=False, description="Constrain to ruptures having a magnitude below the value supplied."
+    )
 
 
 class FilterInversionSolution(graphene.ObjectType):
-
-    class Arguments:
-        input = InversionSolutionAnalysisArguments(required=True)
-
     analysis = graphene.Field(InversionSolutionAnalysis)
-
-    @staticmethod
-    def resolve_analysis(root, info, **args):
-        log.info("FilterInversionSolution.resolve_analysis args: %s" % args)
-        return InversionSolutionAnalysis("ABC", [], [], "" )
 
 
 class QueryRoot(graphene.ObjectType):
@@ -55,14 +63,27 @@ class QueryRoot(graphene.ObjectType):
 
     node = relay.Node.Field()
     about = graphene.String(description='About this Solvis API ')
-    analyse_solution = graphene.Field(FilterInversionSolution, input=graphene.Argument(InversionSolutionAnalysisArguments, required=True)) # description='About this Solvis API ')
+    analyse_solution = graphene.Field(
+        FilterInversionSolution, input=graphene.Argument(InversionSolutionAnalysisArguments, required=True)
+    )  # description='About this Solvis API ')
 
     def resolve_about(root, info, **args):
         return "Hello World, I am solvis_graphql_api!"
 
     def resolve_analyse_solution(root, info, input, **args):
         log.info('args: %s input:%s' % (args, input))
-        return FilterInversionSolution()
-        return "Hello World, I am solvis_graphql_api!"
+        rupture_sections_gdf = matched_rupture_sections_gdf(
+            input['solution_id'],
+            ','.join(input['location_codes']),  # convert to string
+            input['radius_km'] * 1000,
+            min_rate=input.get('minimum_rate') or 1e-20,
+            max_rate=input.get('maximum_rate'),
+            min_mag=input.get('minimum_mag'),
+            max_mag=input.get('maximum_mag'),
+        )
+        log.info('rupture_sections_gdf %s' % rupture_sections_gdf)
+        geojson = gpd.GeoDataFrame(rupture_sections_gdf).to_json(indent=2)
+        return FilterInversionSolution(analysis=InversionSolutionAnalysis(geojson=geojson))
+
 
 schema_root = graphene.Schema(query=QueryRoot, mutation=None, auto_camelcase=False)
--- tests/test_solvis_solution_faults_api.py
+++ tests/test_solvis_solution_faults_api.py
@@ -1,10 +1,44 @@
 """Tests for `solvis_graphql_api` package."""
 
 import unittest
+import json
+from unittest import mock
 from graphene.test import Client
 
-from solvis_graphql_api.schema import schema_root
+from solvis_graphql_api.schema import schema_root  # , matched_rupture_sections_gdf
 from solvis_graphql_api.solvis_graphql_api import create_app
+from pathlib import Path
+import geopandas as gpd
+
+
+def mock_dataframe(*args, **kwargs):
+    with open(Path(Path(__file__).parent, 'fixtures', 'geojson.json'), 'r') as geojson:
+        return gpd.read_file(geojson)
+
+
+QUERY = """
+    query (
+        $solution_id: ID!
+        $location_codes: [String!]
+        $radius_km: Int!
+        )
+    {
+    analyse_solution(
+        input: {
+            solution_id: $solution_id
+            location_codes: $location_codes
+            radius_km: $radius_km
+            }
+        )
+        {
+            analysis {
+                solution_id
+                # fault_sections { fault_id}
+                geojson
+            }
+        }
+    }
+"""
 
 
 class TestFlaskApp(unittest.TestCase):
@@ -15,29 +49,41 @@ class TestFlaskApp(unittest.TestCase):
         print(app)
         assert app
 
+
+@mock.patch('solvis_graphql_api.schema.matched_rupture_sections_gdf', side_effect=mock_dataframe)
 class TestSolutionFaultsResolver(unittest.TestCase):
     """
-    A resolver returns info aobut faults in a given solution inversion (via SolvisStore.
+    A resolver returns info abut faults in a given solution inversion (via SolvisStore.
     """
 
     def setUp(self):
         self.client = Client(schema_root)
 
-    def test_get_about(self):
+    def test_get_analysis(self, mock1):
 
-        QUERY = """
-        query ($solution_id: ID!) {
-          analyse_solution( input: {solution_id: $solution_id} )
-            {
-                analysis {
-                    solution_id
-                    # fault_sections { fault_id}
-                }
-            }
-        }
-        """
+        executed = self.client.execute(
+            QUERY,
+            variable_values={"solution_id": "NANA", "location_codes": ['WLG'], "radius_km": 10},  # this is in PROD !
+        )
+        # print(executed)
 
-        executed = self.client.execute(QUERY, variable_values={"solution_id": "R2VuZXJhbFRhc2s6MQ=="})
-        print(executed)
+        mock1.assert_called_once()
+        mock1.assert_called_once_with('NANA', 'WLG', 10000, min_rate=1e-20, max_rate=None, min_mag=None, max_mag=None)
+
         self.assertTrue('analyse_solution' in executed['data'])
+        self.assertTrue('analysis' in executed['data']['analyse_solution'])
 
+    def test_get_analysis_geojson(self, mock1):
+
+        executed = self.client.execute(
+            QUERY,
+            variable_values={"solution_id": "NANA", "location_codes": ['WLG'], "radius_km": 10},  # this is in PROD !
+        )
+
+        # CBC TODO call loads twice! really ??
+        gj = json.loads(json.loads(executed['data']['analyse_solution']['analysis']['geojson']))
+
+        self.assertTrue('features' in gj)
+        # print(gj.get('features')[0])
+        self.assertTrue('id' in gj['features'][0])
+        self.assertTrue(gj['features'][0]['properties']['id'] == '5')
