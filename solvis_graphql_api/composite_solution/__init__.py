0dcc9fd791afd59beaf25ddaf44b4a7a1d59b914
Chris Chamberlain <chrisbc@artisan.co.nz>
2023-05-03 11:03:38 +1200

    refactor composite_rupture_sections

--- solvis_graphql_api/composite_solution/__init__.py
+++ solvis_graphql_api/composite_solution/__init__.py
@@ -6,4 +6,5 @@ from .composite_rupture_detail import (  # SortRupturesArgs,
     SimpleSortRupturesArgs,
 )
 from .composite_solution import CompositeSolution
-from .schema import paginated_filtered_ruptures, filtered_rupture_sections, CompositeRuptureSections
+from .schema import paginated_filtered_ruptures
+from .composite_rupture_sections import filtered_rupture_sections, CompositeRuptureSections
