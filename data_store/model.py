0a5621f0b741b3c13fb45bd9ff8d40a7ba5df8fc
Chris Chamberlain <chrisbc@artisan.co.nz>
2024-07-08 16:42:37 +1200

    added S3 blob attribute to store

--- data_store/model.py
+++ data_store/model.py
@@ -1,12 +1,13 @@
 import logging
-
+import boto3
+import io
 from pynamodb.attributes import JSONAttribute, UnicodeAttribute
 
 from pynamodb.models import Model, Condition
 
 from typing import Dict, Any, Optional, Sequence
 
-from .config import DEPLOYMENT_STAGE, IS_OFFLINE, REGION, TESTING
+from .config import DEPLOYMENT_STAGE, IS_OFFLINE, REGION, TESTING, S3_BUCKET_NAME
 
 log = logging.getLogger(__name__)
 
@@ -21,6 +22,7 @@ class BinaryLargeObjectModel(Model):
 
     object_id = UnicodeAttribute(hash_key=True)
     object_type = UnicodeAttribute()
+    object_meta = JSONAttribute()
 
     """
     The following is an attempt to intercept the Model classmethods used to read/write models
@@ -46,12 +48,40 @@ class BinaryLargeObject():
     """
     A class wrapping BinaryLargeObjectModel so that we can intercept the save/get operations
 
-    TODO: maybe we can use Model directlym but we have issues with the get() classmethod
-
+    TODO: maybe we can use Model directly but we have issues with the get() classmethod
     """
 
-    def __init__(self, *args, **kwargs):
-        self._model_instance = BinaryLargeObjectModel(*args, **kwargs)
+    def __init__(self, object_id, object_type, object_meta, object_blob, client_args = None):
+        self._model_instance = BinaryLargeObjectModel(
+            object_id=object_id,
+            object_type=object_type,
+            object_meta=object_meta
+        )
+        self._object_blob = object_blob
+        self._bucket_name = S3_BUCKET_NAME
+        self._s3_bucket = None
+        self._s3_conn = None
+        self._s3_client = None
+        self._aws_client_args = client_args or {}
+
+    @property
+    def s3_client(self):
+        if not self._s3_client:
+            self._s3_client = boto3.client('s3', **self._aws_client_args)
+        return self._s3_client
+
+    @property
+    def s3_connection(self):
+        if not self._s3_conn:
+            self._s3_conn = boto3.resource('s3')
+            # self._connection = Connection(region=REGION)
+        return self._s3_conn
+
+    @property
+    def s3_bucket(self):
+        if not self._s3_bucket:
+            self._s3_bucket = self.s3_connection.Bucket(self._bucket_name, client=self.s3_client)
+        return self._s3_bucket
 
     @property
     def object_id(self):
@@ -65,8 +95,15 @@ class BinaryLargeObject():
     def object_meta(self):
         return self._model_instance.object_meta
 
+    @property
+    def object_blob(self):
+        return self._object_blob
+
     def to_json(self):
-        return self._model_instance.to_json()
+        mijson = self._model_instance.to_simple_dict()
+        print(mijson)
+        mijson['object_blob'] = self._object_blob
+        return mijson
 
     @classmethod
     def exists(cls) -> Dict[str, Any]:
@@ -80,14 +117,26 @@ class BinaryLargeObject():
     def delete_table(cls) -> Dict[str, Any]:
         return BinaryLargeObjectModel.delete_table()
 
-
     def save(self) -> Dict[str, Any]:
+        self.s3_bucket.put_object(Key=self.object_id, Body=self.object_blob)
         return self._model_instance.save()
 
     @classmethod
     def get(cls, hash_key: Any, range_key: Optional[Any] = None, consistent_read: bool = False, attributes_to_get: Optional[Sequence[str]] = None) -> Dict[str, Any]:
         model_instance = BinaryLargeObjectModel.get(hash_key, range_key, consistent_read, attributes_to_get)
-        return model_instance
+
+        instance = cls(
+            model_instance.object_id,
+            model_instance.object_type,
+            model_instance.object_meta,
+            None
+        )
+
+        file_object = io.BytesIO()
+        instance.s3_bucket.download_fileobj(instance.object_id, file_object)
+        file_object.seek(0)
+        instance._object_blob = file_object.read()
+        return instance
 
 tables = [BinaryLargeObjectModel]
 
--- data_store/test/test_model.py
+++ data_store/test/test_model.py
@@ -6,11 +6,15 @@ import boto3
 from moto import mock_dynamodb, mock_s3
 # from pynamodb.connection.base import Connection  # for mocking
 
-REGION = 'no-region'
-S3_BUCKET_NAME = 'bogus_bucket'
+# REGION = 'no-region'
+# S3_BUCKET_NAME = 'bogus_bucket'
+
+from data_store.config import DEPLOYMENT_STAGE, IS_OFFLINE, REGION, TESTING, S3_BUCKET_NAME
 
 from data_store import model
 
+
+@mock_s3
 @mock_dynamodb
 class TestBinaryLargeObject():
 
@@ -24,15 +28,21 @@ class TestBinaryLargeObject():
         assert not model.BinaryLargeObject.exists()
 
     def test_create_blob_object(self):
+
+        conn = boto3.resource("s3", region_name=REGION)
+        conn.create_bucket(Bucket=S3_BUCKET_NAME)
+
         model.BinaryLargeObject.create_table()
         myBlob = model.BinaryLargeObject(
             object_id = 'ABC',
             object_type = "MyObjectTypename",
-            object_meta = dict(a=1, b=2)
+            object_meta = dict(a=1, b=2),
+            object_blob = b'0x1234'
         )
         myBlob.save()
         savedBlob = model.BinaryLargeObject.get('ABC')
         # assert 0
         print(savedBlob.to_json())
         assert savedBlob.to_json() == myBlob.to_json()
+        assert savedBlob.object_blob == myBlob.object_blob
 
