db6ccd267e50ca5055696c6018c73faf70c4f3b2
Chris Chamberlain <chrisbc@artisan.co.nz>
2024-07-12 11:30:51 +1200

    remove unused data_store modules;

--- data_store/__init__.py
+++ data_store/__init__.py
@@ -1,5 +1,15 @@
 """
-Module entry point
-"""
+this **data_store** package  may be merged with **solvis_store** eventually or generalised to its own repo.
 
-from .data_manager import get_data_manager
+It provides a hybrid class BinaryLargeObject which uses dynamodb for searching/indexing/identity** features
+and s3 for the blob storage. The blob can be any binary object up to around 1GB.
+
+Much larger file sizes can be supported by S3 but additional (multipart) download options will be needed.
+
+Examples:
+
+```
+TODO
+
+```
+"""
