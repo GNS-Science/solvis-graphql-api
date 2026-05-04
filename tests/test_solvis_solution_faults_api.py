5b7cb754e760e544a3693088e46c659dfec7b554
Oakley Jurgens <o.jurgens@gns.cri.nz>
2026-05-04 03:57:26 +0000

    PR changes

--- tests/test_solvis_solution_faults_api.py
+++ tests/test_solvis_solution_faults_api.py
@@ -10,7 +10,6 @@ import pandas as pd
 import pytest
 from graphene.test import Client
 
-import solvis_graphql_api.solution_schema
 from solvis_graphql_api.schema import schema_root  # , matched_rupture_sections_gdf
 
 
