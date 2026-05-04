41b60199148099e3d47b4df980d1061fdd7ed599
Oakley Jurgens <o.jurgens@gns.cri.nz>
2026-05-04 04:03:52 +0000

    fixes

--- tests/test_solvis_solution_faults_api.py
+++ tests/test_solvis_solution_faults_api.py
@@ -260,8 +260,6 @@ class TestSolutionFaultsResolverExceptions(unittest.TestCase):
         side_effect=mock_dataframe,
     )
     def test_get_analysis_large_dataframe(self, mock1):
-        default_limit = int(solvis_graphql_api.solution_schema.FAULT_SECTION_LIMIT)
-        solvis_graphql_api.solution_schema.FAULT_SECTION_LIMIT = 30
 
         executed = self.client.execute(
             QUERY,
@@ -271,8 +269,6 @@ class TestSolutionFaultsResolverExceptions(unittest.TestCase):
                 "radius_km": 10,
             },  # this is in PROD !
         )
-
-        solvis_graphql_api.solution_schema.FAULT_SECTION_LIMIT = default_limit
 
         self.assertTrue("errors" in executed)
         self.assertTrue("message" in executed["errors"][0])
